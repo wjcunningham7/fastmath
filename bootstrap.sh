b85c4657763d464da0c8f38598ad3d44ddcfb25e
Will Cunningham <wjcunningham7@gmail.com>
2016-06-19 20:23:28 -0400

    Added tools for autoconf

---
+++ bootstrap.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+mkdir -p m4
+touch NEWS AUTHORS ChangeLog
+autoreconf -vfi
--- inc/FastNumInt.h
+++ inc/FastNumInt.h
@@ -6,7 +6,7 @@
 
 #include <gsl/gsl_errno.h>
 #include <gsl/gsl_integration.h>
-#include <nintlib/nintlib.hpp>
+#include <nintlib/nint.hpp>
 
 #define FNI_DEBUG true
 
