47f9fe407673a5ccae962da0730da2279aca30b3
Will Cunningham <wjcunningham7@gmail.com>
2018-01-03 13:16:31 -0500

    Added progressbar

---
+++ inc/progressbar.h
@@ -0,0 +1,29 @@
+#ifndef PROGRESSBAR_H_
+#define PROGRESSBAR_H_
+
+/////////////////////////////
+//(C) Will Cunningham 2018 //
+//         DK Lab          //
+// Northeastern University //
+/////////////////////////////
+
+#include <stdio.h>
+
+struct ProgressBar {
+	ProgressBar() : bar_width(70), steps(0), step(0) {}
+	ProgressBar(int _steps) : bar_width(70), steps(_steps), step(0) {}
+
+	int bar_width;
+	int steps;
+	int step;
+
+	inline float get_progress() { return static_cast<float>(step) / steps; }
+	inline int get_pos() { return bar_width * get_progress(); }
+	inline int get_unit() { return steps / 100; }
+};
+
+void printProgress(ProgressBar &pb);
+void updateProgress(ProgressBar &pb, int step);
+void completeProgress(ProgressBar &pb);
+
+#endif
---
+++ src/progressbar.cpp
@@ -0,0 +1,39 @@
+#include "progressbar.h"
+
+/////////////////////////////
+//(C) Will Cunningham 2018 //
+//         DK Lab          //
+// Northeastern University //
+/////////////////////////////
+
+void printProgress(ProgressBar &pb)
+{
+	printf("[");
+	float pos = pb.get_pos();
+	for (int i = 0; i < pb.bar_width; i++) {
+		if (i < pos) printf("=");
+		else if (i == pos) printf(">");
+		else printf(" ");
+	}
+	printf("] %d %%\r", static_cast<int>(pb.get_progress() * 100));
+	fflush(stdout);
+	return;
+}
+
+void updateProgress(ProgressBar &pb, int step)
+{
+	pb.step = step;
+	if (!(pb.step % pb.get_unit()))
+		printProgress(pb);
+	return;
+}
+
+void completeProgress(ProgressBar &pb)
+{
+	printf("[");
+	for (int i = 0; i < pb.bar_width; i++)
+		printf("=");
+	printf("] 100%%\n");
+	fflush(stdout);
+	return;
+}
