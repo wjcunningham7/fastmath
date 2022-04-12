514652bce71a9320a3e7953ac62695fb432530bd
Will Cunningham <wjcunningham7@gmail.com>
2022-04-11 22:10:29 -0400

    debugging

--- build-rpm-spec.sh
+++ build-rpm-spec.sh
@@ -24,13 +24,30 @@ Requires:	gsl-devel >= 1.13
 %description
 The FastMath toolkit provides efficient numerical approximations and compact data structures
 
+%prep
+%setup -q
+
 %build
 %configure --prefix=/usr/local
 
 %install
 %makeinstall
 
+%files
+%defattr(-,root,root,-)
+/usr/local/include/fastmath/config.h
+/usr/local/include/fastmath/fastapprox.h
+/usr/local/include/fastmath/fastbitset.h
+/usr/local/include/fastmath/fastmath.h
+/usr/local/include/fastmath/integration.h
+/usr/local/include/fastmath/mersenne.h
+/usr/local/include/fastmath/nint.h
+/usr/local/include/fastmath/printcolor.h
+/usr/local/include/fastmath/progressbar.h
+/usr/local/include/fastmath/resources.h
+/usr/local/include/fastmath/stopwatch.h
+
 %changelog
 * Sun Apr 11 2022 Will Cunningham <wjcunningham7@gmail.com> - 0.0.1
 - First package version
-EOF
+
