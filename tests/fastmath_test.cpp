320a13cce1e7d926b7ccd5665485222101f1442d
Will Cunningham <wjcunningham7@gmail.com>
2022-05-07 22:31:14 -0400

    more tests

--- tests/fastmath_test.cpp
+++ tests/fastmath_test.cpp
@@ -12,7 +12,11 @@ TEST (POW2, Pow2)
 {
 	ASSERT_EQ (fastmath::POW2(0, fastmath::EXACT), 0);
 	ASSERT_EQ (fastmath::POW2(0.5, fastmath::EXACT), 0.25);
-	ASSERT_NEAR (fastmath::POW2(0.5, fastmath::FAST), 0.25, 0.0001);
+	ASSERT_EQ (fastmath::POW2(0.5, fastmath::STL), 0.25);
+	ASSERT_NEAR (fastmath::POW2(0.5, fastmath::FAST), 0.25, 0.00001);
+	ASSERT_NEAR (fastmath::POW2(0.5, fastmath::FASTER), 0.25, 0.015);
+	x = fastmath::POW2(0.5, fastmath::DEF);
+	ASSERT_NE (x, x);
 }
 
 int main(int argc, char **argv)
