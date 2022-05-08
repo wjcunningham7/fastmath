8208598e6bb099705c8fd61a8d30124f558ed605
Will Cunningham <wjcunningham7@gmail.com>
2022-05-07 23:14:18 -0400

    adding all invocations

--- tests/fastmath_test.cpp
+++ tests/fastmath_test.cpp
@@ -30,4 +30,40 @@ void instantiate()
 	fastmath::POW2(0);
 	fastmath::POW2(0, fastmath::EXACT);
 	fastmath::POW3(0);
+	fastmath::POW3(0, fastmath::EXACT);
+	fastmath::POW(0, 0, fastmath::STL);
+	fastmath::SQRT(0, fastmath::STL);
+	fastmath::ABS(0, fastmath::STL);
+	fastmath::LOG(1, fastmath::STL);
+	fastmath::SGN(1);
+	fastmath::SGN(1, fastmath::DEF);
+	fastmath::SIN(0, fastmath::STL);
+	fastmath::COS(0, fastmath::STL);
+	fastmath::TAN(0, fastmath::STL);
+	fastmath::ACOS(0, fastmath::STL, fastmath::DEFAULT);
+	fastmath::ATAN(0, fastmath::STL, fastmath::DEFAULT);
+	fastmath::SINH(0, fastmath::STL);
+	fastmath::COSH(0, fastmath::STL);
+	fastmath::ASINH(0, fastmath::STL, fastmath::DEFAULT);
+	fastmath::ACOSH(1, fastmath::STL, fastmath::DEFAULT);
+	fastmath::GAMMA(1);
+	fastmath::GAMMA(1, fastmath::STL);
+	fastmath::LOGGAMMA(1);
+	fastmath::LOGGAMMA(1, fastmath::STL);
+	fastmath::GAMMA_RATIO(1, 1);
+	fastmath::POCHHAMMER(1, 0);
+	fastmath::POCHHAMMER_F(1, 0);
+	fastmath::_2F1_An_F(1, 1, 1, 1);
+	fastmath::_2F1_An(1, 1, 1, 1);
+
+	double sol, err;
+	int nterms = 1;
+	fastmath::_2F1(1, 1, 1, 1, &sol, &err, &nterms, false);
+	fastmath::_2F1_F(1, 1, 1, 1, &sol, &err, &nterms);
+
+	double z = 0;
+	err = 0.1;
+	fastmath::getNumTerms(z, err);
+
+	fastmath::getHyperType(z);
 }
